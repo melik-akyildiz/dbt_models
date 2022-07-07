a94c54bf332cbfdb20670e2c91c3e1e3d9045ab8
melikjava <melik.akyildiz@hotmail.com>
2022-07-07 13:34:17 +0200

    unique ket removed because of one-to-many relationship

--- models/collibra/ODSS/MELIK_CUSTOMER.sql
+++ models/collibra/ODSS/MELIK_CUSTOMER.sql
@@ -1,5 +1,5 @@
 
-/*
+/*  version_test:1
     Welcome to your first dbt model!
     Did you know that you can also configure models directly within SQL files?
     This will override configurations stated in dbt_project.yml
