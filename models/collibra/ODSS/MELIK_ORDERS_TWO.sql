246083fb0cf59516764023eb535c957bf70958de
melikjava <melik.akyildiz@hotmail.com>
2022-07-28 13:05:25 +0200

    adding new models 3

--- models/collibra/ODSS/MELIK_ORDERS_TWO.sql
+++ models/collibra/ODSS/MELIK_ORDERS_TWO.sql
@@ -1,27 +1,5 @@
 
-/*  version_test:1
-    Welcome to your first dbt model!
-    Did you know that you can also configure models directly within SQL files?
-    This will override configurations stated in dbt_project.yml
+-- Use the `ref` function to select from other models
 
-    Try changing "table" to "view" below
-*/
-
-{{ config(materialized='table') }}
-
-with MELIK_CUSTOMER as (
-
-    select 11 as C_CUSTKEY
-    union all
-    select null as C_CUSTKEY
-
-)
-
-select *
-from MELIK_CUSTOMER where C_CUSTKEY=11
-
-/*
-    Uncomment the line below to remove records with null `id` values
-*/
-
--- where id is not null
+select C_CUSTKEY
+from {{ ref('MELIK_CUSTOMER') }}
