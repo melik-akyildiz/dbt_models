3f02a7ed93646ec8e2fe101ecabd987db7fe9245
melikjava <melik.akyildiz@hotmail.com>
2022-08-24 12:37:19 +0200

    sql changes

---
+++ models/collibra/ODSS/METRICS_EXAMPLE.sql
@@ -0,0 +1,9 @@
+
+-- Use the `ref` function to select from other models
+
+select *
+from {{ metrics.metric(
+            metric_name='test_metrics',
+            grain='month',
+            dimensions=['priority_code']
+    ) }}
