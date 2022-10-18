944b43dc1d90455c4f5ee8d11a7faa6b6195d42a
melikjava <melik.akyildiz@hotmail.com>
2022-10-18 16:36:00 +0200

    metrics fix

--- models/collibra/ODSS/DEPENDS_ON_EXAMPLE.sql
+++ models/collibra/ODSS/DEPENDS_ON_EXAMPLE.sql
@@ -1,5 +1,5 @@
 
 -- Use the `ref` function to select from other models
 
-select C_CUSTKEY
+select *
 from {{ ref('MAIN_EXAMPLE') }}
--- models/collibra/ODSS/SOURCE_EXAMPLE.sql
+++ models/collibra/ODSS/SOURCE_EXAMPLE.sql
@@ -1,6 +1,6 @@
 
 -- Use the `ref` function to select from other models
 
-select C_CUSTKEY
+select *
 from {{ source('test_source', 'MELIK_CUSTOMER') }}
 
